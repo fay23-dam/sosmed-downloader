22df20f800983edf33e862c392fb0037b4cc011c
fay23-dam <saddansatria23@gmail.com>
2024-10-31 06:37:58 -0700

    second commit

--- index.js
+++ index.js
@@ -70,5 +70,5 @@ app.post('/download', async (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
