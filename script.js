9d6e6c1250c25901fa78cd89de043c1d800ac0cc
fay23-dam <saddansatria23@gmail.com>
2024-10-31 07:02:18 -0700

    Add got module as dependency

--- script.js
+++
@@ -1,9 +0,0 @@
-const {instagram} = require("nayan-media-downloader");
-
-const link = "https://www.instagram.com/p/C6jPgiixcJj/?utm_source=ig_web_copy_link" //past video link
-
-  instagram(link).then(data => {
-
-    console.log(data)
-
-});
